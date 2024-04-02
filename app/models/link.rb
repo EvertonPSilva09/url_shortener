454e301498bab1fead9bd5a5e96d65aac6be9c14
Everton Silva <everton.p.silva@outlook.com>
2024-04-01 22:56:43 -0300

    feat: add validation for URL in Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,14 @@
+# Table name: links
+#
+#  id          :bigint           not null, primary key
+#  url         :string
+#  title       :string
+#  description :string
+#  image       :string
+#  views_count :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Link < ApplicationRecord
+  validates :url, presence: true
 end
