e0692b2a62ece21240ea0054bc498abfc5618b5d
Everton Silva <everton.p.silva@outlook.com>
2024-04-05 22:58:12 -0300

    feat: add to_param method to Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -13,4 +13,8 @@ class Link < ApplicationRecord
   scope :recent_first, -> { order(created_at: :desc) }
   
   validates :url, presence: true
+  
+  def to_param
+    ShortCode.encode(id)
+  end
 end
