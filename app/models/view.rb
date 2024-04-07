52b48092773416d50f08479e7445888cb191dcc5
Everton Silva <everton.p.silva@outlook.com>
2024-04-06 23:42:30 -0300

    feat: add View model with associations

---
+++ app/models/view.rb
@@ -0,0 +1,12 @@
+# Table name: views
+#
+# link_id    :bigint           not null
+# user_agent :string
+# ip         :string
+# created_at :datetime         not null
+# updated_at :datetime         not null
+#
+
+class View < ApplicationRecord
+  belongs_to :link, counter_cache: true
+end
