6bf73f9264d69a204a86cfefec848d26f7e537e7
Everton Silva <everton.p.silva@outlook.com>
2024-04-10 22:45:52 -0300

    feat: add MetadataTest to test title attribute and nil title attribute

---
+++ test/models/metadata_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class MetadataTest < ActiveSupport::TestCase
+  test "title attribute" do
+    assert_equal "Hello", Metadata.new("<title>Hello</title>").title
+  end
+ 
+  test "nil title attribute" do
+    assert_nil Metadata.new.title
+  end
+end
