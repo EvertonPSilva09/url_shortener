5f73fbb900938d062b4c32066e63a376c1b7fb51
Everton Silva <everton.p.silva@outlook.com>
2024-04-10 23:06:54 -0300

    change: update image metadata tag in MetadataTest

--- test/models/metadata_test.rb
+++ test/models/metadata_test.rb
@@ -18,7 +18,7 @@ class MetadataTest < ActiveSupport::TestCase
   end
 
   test "image" do
-    assert_equal "https://example.org/favicon.ico", Metadata.new("<meta name='og:image' content='https://example.org/favicon.ico'>").image
+    assert_equal "https://example.org/favicon.ico", Metadata.new("<meta property='og:image' content='https://example.org/favicon.ico'>").image
   end
 
   test "missing image" do
