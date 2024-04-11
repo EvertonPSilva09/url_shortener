24f5f73ff6c12188609cdd1fdb45711c8115f8ee
Everton Silva <everton.p.silva@outlook.com>
2024-04-10 22:57:08 -0300

    feat: add image attribute to Metadata model tests

--- test/models/metadata_test.rb
+++ test/models/metadata_test.rb
@@ -4,16 +4,24 @@ class MetadataTest < ActiveSupport::TestCase
   test "title attribute" do
     assert_equal "Hello", Metadata.new("<title>Hello</title>").title
   end
- 
+
   test "missing title attribute" do
     assert_nil Metadata.new.title
   end
-  
+
   test "meta description" do
     assert_equal "Hi! im a description", Metadata.new("<meta name='description' content='Hi! im a description'>").description
   end
- 
+
   test "missing meta description" do
     assert_nil Metadata.new.description
+  end
+
+  test "image" do
+    assert_equal "https://example.org/favicon.ico", Metadata.new("<meta name='og:image' content='https://example.org/favicon.ico'>").image
+  end
+
+  test "missing image" do
+    assert_nil Metadata.new.image
   end
 end
