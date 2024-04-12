98c394e2a88baca7b7434547846e4dd8eb2ab141
Everton Silva <everton.p.silva@outlook.com>
2024-04-11 23:13:19 -0300

    change: refactor autofocus selector in reset_form_controller.js

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   reset() {
     this.element.reset()
-    this.element.querySelector("[autofocus='autofocus']").focus()
+    this.element.querySelector("[autofocus='autofocus']")?.focus()
   }
 }
