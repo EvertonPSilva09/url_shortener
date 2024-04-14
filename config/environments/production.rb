def4b18943042bc3196bb39a81755eb45faebfb4
Everton Silva <everton.p.silva@outlook.com>
2024-04-13 23:40:26 -0300

    feat: add Sucker Punch as the queue adapter for active job

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Sucker Punch
+  config.active_job.queue_adapter = :sucker_punch
 end
